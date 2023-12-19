43057d5bcf647f068f96d561988910d8c193906f
Danial <fsoocity@gmail.com>
2023-12-19 19:23:51 +0330

    feat():Create Footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero/Hero";
 import AboutMe from "./components/AboutMe/AboutMe";
 import Skills from "./components/Skills/Skills";
 import Projects from "./components/Projects/Projects";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <AboutMe />
       <Skills />
       <Projects />
+      <Footer />
     </Stack>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,53 @@
+import { Container, Fab, IconButton, Stack, Typography } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TelegramIcon from "@mui/icons-material/Telegram";
+import ArrowCircleUpIcon from "@mui/icons-material/ArrowCircleUp";
+const Icons = [<GitHubIcon />, <InstagramIcon />, <TelegramIcon />];
+
+const Footer = () => {
+  return (
+    <Stack width="100%" position="relative">
+      <Container
+        sx={{
+          py: "30px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: "20px",
+        }}
+      >
+        <Stack flexDirection="row" color="primary.main" gap="10px">
+          {Icons.map((icon) => (
+            <IconButton
+              sx={{
+                p: "6px 8px",
+                bgcolor: "white",
+                borderRadius: "12px",
+                boxShadow: 3,
+              }}
+              color="inherit"
+            >
+              {icon}
+            </IconButton>
+          ))}
+        </Stack>
+        <Typography>©2023 | All Rights Reserved</Typography>
+      </Container>
+      <Fab
+        color="primary"
+        sx={{ position: "absolute", right: "10px", bottom: "10px" }}
+        onClick={() => {
+          scrollTo({
+            top: 0,
+            behavior: "smooth",
+          });
+        }}
+      >
+        <ArrowCircleUpIcon />
+      </Fab>
+    </Stack>
+  );
+};
+
+export default Footer;
